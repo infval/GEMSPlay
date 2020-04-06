c5850b90d0cd1c061f1c859a6cf2c3a1b51cdb8b
infvaL <38145742+infval@users.noreply.github.com>
2020-04-06 19:13:35 +0300

    Fix typos (ValleyBell)

--- GEMSPlayLibrary/GemsPlay.c
+++ GEMSPlayLibrary/GemsPlay.c
@@ -109,7 +109,7 @@ static struct
 	UINT8 psgdec[4];		// [0015] 12 decay rate
 	UINT8 psgslv[4];		// [0019] 16 sustain level attenuation
 	UINT8 psgrrt[4];		// [001D] 20 release rate
-	UINT8 psgenv[4];		// [0021] 24 envelope mode 0 = off, 1 = attack, 2 = decay, 3 = sustain, 4
+	UINT8 psgenv[4];		// [0021] 24 envelope mode 0 = off, 1 = attack, 2 = decay, 3 = sustain, 4 = release
 	UINT8 psgdtl[4];		// [0025] 28 tone bottom 4 bits, noise bits
 	UINT8 psgdth[4];		// [0029] 32 tone upper 6 bits
 	UINT8 psgalv[4];		// [002D] 36 attack level attenuation
@@ -1322,8 +1322,8 @@ static UINT8 seqcmd(UINT8* ChnCCB, UINT8* CHBUFPTR, UINT8 CurChn, UINT8 CmdByte)
 			INT16 JmpOfs;
 			UINT32 FinalAddr;
 			
-			JmpOfs = GETSBYTE(ChnCCB, CHBUFPTR);
-			JmpOfs = (GETSBYTE(ChnCCB, CHBUFPTR) << 8) | (JmpOfs << 0);
+			CurArg = GETSBYTE(ChnCCB, CHBUFPTR);
+			JmpOfs = (GETSBYTE(ChnCCB, CHBUFPTR) << 8) | (CurArg << 0);
 			//seqbranch:					// jump to addr + 24 bit offset in DHL
 			FinalAddr = Read24Bit(&ChnCCB[CCBADDRL]);
 			FinalAddr += JmpOfs;
@@ -1886,7 +1886,7 @@ void STARTSEQ(UINT8 SeqNum)
 	UINT8 DoGems28;
 	
 	StartSignal(SeqNum);
-	SBPTACC = 0;		// [note in actual code] reset current tempo ticks to prevent bugs
+	SBPTACC = 0;		// [not in actual code] reset current tempo ticks to prevent bugs
 	
 	stseqsnum = SeqNum;
 	STblPtr = Read24Bit(Tbls.STBL68K);		// [not in actual code] caching for later use
@@ -2285,7 +2285,7 @@ static void DOENVELOPE(void)
 	return;
 }
 
-// DOPITCHBEND- updates the (pitchbend) value for the gems channel (= MIDI channel during perf [another cut comment]
+// DOPITCHBEND- updates the (pitchbend) value for the gems channel (= MIDI channel during perf mode)
 //
 //		inputs:		A							CCB number (0-15)
 //					(next 2 bytes in cmd queue)	pbend value
@@ -2627,7 +2627,7 @@ static void noteondig(UINT8 MidChn, UINT8* ChnCCB)
 											//   VoiceTable AND Envelope Trigger
 	FMVTBLCH6[VTBLFLAGS] |= 0x20;			// lock the voice from FM allocation [SET #5]
 	
-	// at this point, C is note number - C4 >> B7 equals samples  0 through 47 (for back compatibil
+	// at this point, C is note number - C4 >> B7 equals samples  0 through 47 (for back compatibility)
 	//									 C0 >> B3 equals samples 48 through 96
 	// trigger sample by reading sample bank table for header
 	CurSmpl = noteon.note;	// [actually, it's read from Register C, which is unchanged from NOTEON]
@@ -2650,7 +2650,7 @@ static void noteondig(UINT8 MidChn, UINT8* ChnCCB)
 	}
 	
 	//sampleok:
-	// now check for sample playback rate override (2nd byte of patch != 4) - override rate in SAMP
+	// now check for sample playback rate override (2nd byte of patch != 4) - override rate in SAMPFLAGS
 	if (CHPATPTR[1] != 4)
 	{
 		SAMP.FLAGS &= 0xF0;					// replace counter value in flags (controls freq)
@@ -2916,7 +2916,7 @@ static void VTANDET(UINT8* ChnCCB, UINT8* VTblPtr, UINT8 VFlags)
 	
 	DACxME();
 	
-	if (ChnCCB[CCBFLAGS] & 0x40)	// envelope retrigger on?
+	if (ChnCCB[CCBFLAGS] & 0x40)	// envelope retrigger on? [BIT #6]
 	{
 		// yes - trigger the envelope
 		TRIGENV(ChnCCB, noteon.ch, ChnCCB[CCBENV]);
@@ -3071,10 +3071,12 @@ static void PATCHLOAD(UINT8 InsNum)
 //					HL	pointer to entry allocated
 static UINT8 ALLOC(UINT8 MidChn, UINT8* ChnCCB, UINT8* VTblPtr, UINT8** RetAlloc)
 {
-	UINT8* avlowestp = NULL;	// [17DA] pointer to lowest priority
-	UINT8* avfreestp = NULL;	// [17DC] pointer to longest free
+	UINT8* avlowestp;	// [17DA] pointer to lowest priority
+	UINT8* avfreestp;	// [17DC] pointer to longest free
 	UINT8 CurPrio;		// Register C
 	UINT8 CurFree;		// Register L
+	
+	avlowestp = avfreestp = NULL;
 	
 	CurPrio = 0xFF;		// lowest prio so far (max actually 7FH)
 	CurFree = 0xFF;		// freest so far (max actually 0FE)
